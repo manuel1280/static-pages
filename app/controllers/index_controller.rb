ba370c3c358d8091cfb2f82cfa59c6f6dfafd5e6
unknown <komodocr7@gmail.com>
2019-09-02 14:52:39 -0500

    create new views and configuration routes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,16 @@
 class IndexController < ApplicationController
   def home
   end
+
+  def basics
+    
+  end
+
+  def ruby
+    
+  end
+
+  def git
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'index#home'
+
+  get '/basics', to: 'index#basics'
+  get '/ruby', to: 'index#ruby'
+  get '/git', to: 'index#git'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
