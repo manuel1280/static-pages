9417627b25b99083f7f97a08af1aa2a5324e0642
unknown <komodocr7@gmail.com>
2019-09-02 15:32:21 -0500

    add time an date on home page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,7 @@
 class IndexController < ApplicationController
   def home
+    @current_time = Time.now.strftime("%H:%M:%S")
+    @current_date = Time.now.strftime("%d-%m-%Y")
   end
 
   def basics
